195c8d96be305fc977b054bbd961098499fa2fe0
okorkov <alex.okorkov@gmail.com>
2020-11-10 16:42:11 -0800

    commit tree d61422bab7af68b36fbacd0d2670d7f7bf291eb1

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+
+def prime?(num)
+    return false if num <= 1
+    n = 2
+    while n < num
+      return false if num % n == 0
+      n += 1
+    end
+    true
+end
+
+puts prime?(67)
